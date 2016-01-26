077e076e7866b9fa044d5607e138e4eacc7d82e4
Pedro Martins <pedromartins.pt@gmail.com>
2016-01-26 15:19:08 +0000

    Switch to msgpack-rpc

--- src/Scaffold/Main.hs
+++ src/Scaffold/Main.hs
@@ -1,17 +1,20 @@
 
 import Control.Concurrent
 import Control.Monad
+import Control.Monad.IO.Class
 
 import System.Posix.Daemonize
 import System.Process
 import System.IO
 import System.Environment
 
+import Network.MessagePack.Client
+
+import qualified Data.ByteString.Char8 as B
+
 import Scaffold.Types
 import Scaffold.Register
-
-import Network.XmlRpc.Client
-import Network.XmlRpc.Internals
+import Scaffold.Util
 
 main = do
   ip <- getIp
@@ -24,9 +27,10 @@ main = do
     -- TODO: get ip (dynip) of registry
     (_, oh, _, _) <- runInteractiveCommand "curl http://www.doc.ic.ac.uk/~pm1108/scaffold/dynIP"
     r <- hGetContents oh
-    let remoteRegister = remote ("http://" ++ r ++ ":80/~pedromartins/scnd.cgi") "register" :: NodeCapRecord -> IO ()
-    (sensors, actuators) <- readConfig
-    mapM (\(s,sd,u) -> remoteRegister (NodeCapRecord (Just ip) (Provides s) sd u)) sensors
-    mapM (\(c,sc,u) -> remoteRegister (NodeCapRecord (Just ip) (IsCapableOf c) sc u)) actuators
-    threadDelay 6000000
+    execClient (B.pack r) 5000 $ do
+      let remoteRegister = call "register" :: NodeCapRecord -> Client Int
+      (sensors, actuators) <- liftIO readConfig
+      mapM (\(s,sd,u) -> remoteRegister (NodeCapRecord (Just ip) (Provides s) sd u)) sensors
+      mapM (\(c,sc,u) -> remoteRegister (NodeCapRecord (Just ip) (IsCapableOf c) sc u)) actuators
+      liftIO $ threadDelay 6000000
 
