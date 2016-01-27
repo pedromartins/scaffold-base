eea5fd4ba145c03936386ebd8be48abf12276229
Pedro Martins <pedromartins.pt@gmail.com>
2016-01-27 16:55:13 +0000

    Daemon metadata

--- src/Scaffold/Main.hs
+++ src/Scaffold/Main.hs
@@ -3,6 +3,7 @@ import Control.Concurrent
 import Control.Monad
 
 import System.Posix.Daemonize
+import qualified System.Posix.Daemonize as SPD
 import System.Process
 import System.IO
 import System.Environment
@@ -20,14 +21,18 @@ main = do
         case args of
           []       -> serviced
           isTest:_ -> if isTest == "-t" then ($()) . program else serviced
-  serviced' $ simpleDaemon { program = \_ -> forever $ do
-    -- TODO: get ip (dynip) of registry
-    (_, oh, _, _) <- runInteractiveCommand "curl http://www.doc.ic.ac.uk/~pm1108/scaffold/dynIP"
-    r <- hGetContents oh
-    let remoteRegister = remote ("http://" ++ r ++ ":80/~pedromartins/scnd.cgi") "register" :: NodeCapRecord -> IO ()
-    (sensors, actuators) <- readConfig
-    mapM (\(s,sd,u) -> remoteRegister (NodeCapRecord (Just ip) (Provides s) sd u)) sensors
-    mapM (\(c,sc,u) -> remoteRegister (NodeCapRecord (Just ip) (IsCapableOf c) sc u)) actuators
-    threadDelay 6000000
-  }
+  serviced' $ simpleDaemon
+    { program = \_ -> forever $ do
+        -- TODO: get ip (dynip) of registry
+        (_, oh, _, _) <- runInteractiveCommand "curl http://www.doc.ic.ac.uk/~pm1108/scaffold/dynIP"
+        r <- hGetContents oh
+        let remoteRegister = remote ("http://" ++ r ++ ":80/~pedromartins/scnd.cgi") "register" :: NodeCapRecord -> IO ()
+        (sensors, actuators) <- readConfig
+        mapM (\(s,sd,u) -> remoteRegister (NodeCapRecord (Just ip) (Provides s) sd u)) sensors
+        mapM (\(c,sc,u) -> remoteRegister (NodeCapRecord (Just ip) (IsCapableOf c) sc u)) actuators
+        threadDelay 6000000
+    , name = Just "scaffold"
+    , SPD.user = Just "scaffold"
+    , group = Just "users"
+    }
 
