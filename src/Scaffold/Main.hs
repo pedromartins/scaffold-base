b5437090aba5eb6756e31dff84d47ab27831ed9b
Pedro Martins <pedromartins.pt@gmail.com>
2016-01-27 16:08:05 +0000

    serviced daemon

--- src/Scaffold/Main.hs
+++ src/Scaffold/Main.hs
@@ -16,11 +16,11 @@ import Network.XmlRpc.Internals
 main = do
   ip <- getIp
   args <- getArgs
-  let daemonize' =
+  let serviced' =
         case args of
-          []       -> daemonize
-          isTest:_ -> if isTest == "-t" then id else daemonize
-  daemonize' . forever $ do
+          []       -> serviced
+          isTest:_ -> if isTest == "-t" then ($()) . program else serviced
+  serviced' $ simpleDaemon { program = \_ -> forever $ do
     -- TODO: get ip (dynip) of registry
     (_, oh, _, _) <- runInteractiveCommand "curl http://www.doc.ic.ac.uk/~pm1108/scaffold/dynIP"
     r <- hGetContents oh
@@ -29,4 +29,5 @@ main = do
     mapM (\(s,sd,u) -> remoteRegister (NodeCapRecord (Just ip) (Provides s) sd u)) sensors
     mapM (\(c,sc,u) -> remoteRegister (NodeCapRecord (Just ip) (IsCapableOf c) sc u)) actuators
     threadDelay 6000000
+  }
 
