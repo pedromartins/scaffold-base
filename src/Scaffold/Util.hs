647673465674cb373e37c428b693a9d1219bba68
Pedro Martins <pedro.martins08@imperial.ac.uk>
2016-04-12 07:08:25 +0100

    Removed mDNS-SD temporarily.

--- src/Scaffold/Util.hs
+++ src/Scaffold/Util.hs
@@ -1,10 +1,8 @@
 module Scaffold.Util where
 
 import Network.Socket
-import Network.DNSSD
 
 import Scaffold.Types
-import Scaffold.DNSSD
 import Scaffold.Config
 
 depreqToCaps :: DepReq -> [DepReq]
@@ -12,13 +10,4 @@ depreqToCaps (d `And` d') = (depreqToCaps d ++ depreqToCaps d')
 depreqToCaps d = [d]
 
 getRegistry :: IO String
-getRegistry = do
-  mr <- resolveOne "_scaffold._tcp"
-  case mr of
-    Nothing -> readRegistryConfig
-    Just (SResolved fn ht p t) -> do
-      h <- fmap (getAddrHost . addrAddress . head) $ getAddrInfo (Just defaultHints { addrFamily = AF_INET }) (Just ht) Nothing
-      return $ "http://" ++ h ++ ":" ++ (show p) ++ "/"
-
-getAddrHost = takeWhile (/=':') . show
-
+getRegistry = readRegistryConfig
